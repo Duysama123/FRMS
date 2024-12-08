c223297579ff46282db075753785ba467a82d58c
Hieu Nguyen <nxhieu2004@gmail.com>
2024-12-08 17:04:36 +0700

    Update data validation for account.rb

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,5 +5,22 @@ has_many :shopping_lists
  # Phương thức để lấy tên các associations
  def self.association_names
   self.reflect_on_all_associations.map(&:name)
+
+   self.reflect_on_all_associations.map(&:name)
+  validates_presence_of :username, :email, :password
+  
+  validates_uniqueness_of :username, :email, :password
+    
+  validates_length_of :username, :email, :password, maximum: 50
+    
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+    
+  after_initialize :set_default_role, if: :new_record?
+    
+  private
+    
+  def set_default_role
+        self.role ||= 'user'
+  end
 end
 end
