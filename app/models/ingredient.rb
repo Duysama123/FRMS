58cd3af79777492e51fb44482d3a561af49350dc
Hieu Nguyen <nxhieu2004@gmail.com>
2024-12-08 17:07:56 +0700

    Update data validation for ingredient.rb

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,9 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :recipe_ingredients
   has_many :shopping_lists
   has_one :nutrition_info
+
+  has_one_attached: image
+  validates_presence_of :name, :description 
+   
+  validates_length_of :name, maximum: 50
 end
