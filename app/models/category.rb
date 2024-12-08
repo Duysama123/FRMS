95ec43a966d2a39181d196abd809b43ab45d98dd
Hieu Nguyen <nxhieu2004@gmail.com>
2024-12-08 17:17:05 +0700

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_many :recipe_categories
   has_many :recipes, through: :recipe_categories
-
+  
   validates_presence_of :name
   validates_length_of :name, maximum: 50
 end
